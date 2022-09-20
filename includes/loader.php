b2dfd6ff0cc6b5c5b2f1df3ad91a07dd4df90fb0
Victor Valera <vvalera@conexia.com>
2022-09-20 18:19:08 -0300

    Call principal functions and shortcode

--- includes/loader.php
+++ includes/loader.php
@@ -1,5 +1,7 @@
 <?php
 
-add_action( 'init', 'recipes_post_type' );
-add_action( 'init', 'create_recipes_taxonomy', 0 );
+add_action( 'init', 'dfcf7_post_type' );
+add_action( 'init', 'create_groups_dfcf7_taxonomy', 0 );
+add_action( 'save_post', 'save_url_data');
 
+add_shortcode('get-download-checkbox', 'make_download');
